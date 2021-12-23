1a7e5eb07587954eba273bf5abc8fec48d684b6f
Enzo Giacomelli <egiacomelli09@gmail.com>
2021-12-23 19:00:00 -0300

    melhorias nos testes de login.

--- desafioFrontTest/Testes de Login/Testes.cs
+++ desafioFrontTest/Testes de Login/Testes.cs
@@ -4,31 +4,29 @@ using OpenQA.Selenium;
 using desafioFrontTest.Page_Object;
 
 
-
 namespace desafioFrontTest.Testes_de_Login
 {
     public class Testes
     {
         private IWebDriver driver;
         private string url = "https://www.saucedemo.com/";
-
+        MapeamentoLogin login;
+        Usuarios user;
 
         [SetUp]
         public void Setup()
         {
             driver = Comandos.ComandosNavegador.abrirNavegador();
+            login = new MapeamentoLogin(driver);
+            user = new Usuarios();
         }
 
         [Test]
         public void loginBlocked()
         {
             driver.Navigate().GoToUrl(url);
-
-            MapeamentoLogin login = new MapeamentoLogin(driver);
-
             login.VerificaElementosNaPagina();
-            Usuarios user = new Usuarios().usuarioIncorreto();
-            login.LogIn(user);
+            login.LogIn(user.usuarioIncorreto());
 
             Assert.IsTrue(login.alertaErro.Text.Contains("Epic sadface: Sorry, this user has been locked out."));
             Console.WriteLine(login.alertaErro.Text);
@@ -38,63 +36,13 @@ namespace desafioFrontTest.Testes_de_Login
         public void loginSuccess()
         {
             driver.Navigate().GoToUrl(url);
-
-            MapeamentoLogin login = new MapeamentoLogin(driver);
-
             login.VerificaElementosNaPagina();
-            Usuarios user = new Usuarios().usuarioCorreto();
-            login.LogIn(user);
+            login.LogIn(user.usuarioCorreto());
 
             Assert.IsTrue(driver.Url == "https://www.saucedemo.com/inventory.html");
             Assert.IsTrue(driver.Title == "Swag Labs");
             Console.WriteLine(driver.Url);
             Console.WriteLine(driver.Title);
         }
-
-        //[Test]
-        /*public void compraCompleta()
-        {
-            driver.Navigate().GoToUrl(url);
-            MapeamentoLogin login = new MapeamentoLogin(driver);
-            MapeamentoInventario inventario = new MapeamentoInventario(driver);
-            MapeamentoCheckout checkout = new MapeamentoCheckout(driver);
-            MapeamentoCarrinho carrinho = new MapeamentoCarrinho(driver);
-
-            login.VerificaElementosNaPagina();
-            login.LogIn("standard_user", "secret_sauce");
-
-            inventario.VerificaElementosNaPagina();
-            inventario.AddToCart();
-
-            carrinho.VerificaElementosNaPagina();
-            carrinho.CheckoutClick();
-
-            checkout.VerificaElementosNaPagina();
-            checkout.PreencheCheckout("Enzo", "Giacomelli", "987654321");
-            checkout.VerificaCompra("Total: $97.17");
-        }
-
-        [Test]
-        public void validarValorTotal()
-        {
-            driver.Navigate().GoToUrl(url);
-            MapeamentoLogin login = new MapeamentoLogin(driver);
-            MapeamentoInventario inventario = new MapeamentoInventario(driver);
-            MapeamentoCheckout checkout = new MapeamentoCheckout(driver);
-            MapeamentoCarrinho carrinho = new MapeamentoCarrinho(driver);
-
-            login.VerificaElementosNaPagina();
-            login.LogIn("standard_user", "secret_sauce");
-
-            inventario.VerificaElementosNaPagina();
-            inventario.AddAllToCart();
-
-            carrinho.VerificaElementosNaPagina();
-            carrinho.CheckoutClick();
-
-            checkout.VerificaElementosNaPagina();
-            checkout.PreencheCheckout("Enzo", "Giacomelli", "987654321");
-            checkout.VerificaCompra("Total: $140.34");
-        }*/
     }
 }
